80eb86d178900fb22af71db3f19c112537adee40
shamratPG <shamratpg@gmail.com>
2022-08-29 13:37:05 +0600

    added new functionality which shows new user for btn click

---
+++ js/scritp.js
@@ -0,0 +1,22 @@
+const getData = () => {
+    fetch('https://randomuser.me/api/?results=100')
+        .then(res => res.json())
+        .then(data => {
+            let obj = randomObj(data.results);
+            changeData(obj)
+        })
+}
+
+const randomObj = ary => {
+    let item = Math.floor(Math.random() * ary.length);
+    return ary[item]
+}
+
+const changeData = obj => {
+    document.getElementById('full-name').innerText = `${obj.name.first} ${obj.name.last}`
+    document.getElementById('pic').setAttribute('src', `${obj.picture.thumbnail}`)
+    document.getElementById('user-name').innerText = `${obj.login.username}`
+    document.getElementById('email').innerText = `${obj.email}`
+}
+
+getData()
